8412296cc402735fca8c5f2c76a20e430c0e319e
banffjiang <banffjiang327@gmail.com>
2025-01-21 15:37:32 -0800

    Update global.js

--- global.js
+++ global.js
@@ -1,15 +1,14 @@
 let pages = [
     { url: '', title: 'Home' },
-    { url: 'projects/', title: 'Projects' },
-    { url: 'contact/', title: 'Contact' },
-    { url: 'cv/', title: 'CV' },
+    { url: 'projects/index.html', title: 'Projects' },  
+    { url: 'contact/index.html', title: 'Contact' },     
+    { url: 'cv/index.html', title: 'CV' },               
     { url: 'https://github.com/banffjiang', title: 'GitHub' }
   ];
-  
-  // Check if we're on the home page
+
   const ARE_WE_HOME = document.documentElement.classList.contains('home');
   
-  // Create the <nav> element
+
   let nav = document.createElement('nav');
   document.body.prepend(nav);
   
@@ -17,14 +16,13 @@ let pages = [
     let url = p.url;
     let title = p.title;
   
-    // Prepend '../' for non-home pages (relative URLs)
+
     url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
   
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
-  
-    // Highlight the current page link
+ 
     if (a.host === location.host && a.pathname === location.pathname) {
       a.classList.add('current');
     }
