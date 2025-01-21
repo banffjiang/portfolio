d22b9333d38c85229eb82095fb0547001286ad10
banffjiang <banffjiang327@gmail.com>
2025-01-21 15:58:31 -0800

    Update global.js

--- global.js
+++ global.js
@@ -1,32 +1,31 @@
 let pages = [
     { url: '', title: 'Home' },
-    { url: 'projects/index.html', title: 'Projects' },  
-    { url: 'contact/index.html', title: 'Contact' },     
-    { url: 'cv/index.html', title: 'CV' },               
+    { url: 'projects/index.html', title: 'Projects' },
+    { url: 'contact/index.html', title: 'Contact' },
+    { url: 'cv/index.html', title: 'CV' },
     { url: 'https://github.com/banffjiang', title: 'GitHub' }
-  ];
+];
 
-  const ARE_WE_HOME = document.documentElement.classList.contains('home');
-  
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
-  let nav = document.createElement('nav');
-  document.body.prepend(nav);
-  
-  for (let p of pages) {
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
     let url = p.url;
     let title = p.title;
-  
 
-    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
-  
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = 'portfolio/' + url;
+    }
+
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
- 
+
     if (a.host === location.host && a.pathname === location.pathname) {
-      a.classList.add('current');
+        a.classList.add('current');
     }
-  
+
     nav.append(a);
-  }
-  
+}
