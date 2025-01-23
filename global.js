889c6e3f91373b48329715381865778d812da07a
banffjiang <banffjiang327@gmail.com>
2025-01-23 14:09:06 -0800

    js

--- global.js
+++ global.js
@@ -31,3 +31,26 @@ for (let p of pages) {
 
     nav.append(a);
 }
+
+document.body.insertAdjacentHTML(
+    'afterbegin', 
+    `
+    <label class="color-scheme">
+      Theme:
+      <select id="color-scheme-switch">
+        <option value="auto" selected>Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+    `
+  );
+
+const select = document.querySelector('#color-scheme-switch');
+
+select.addEventListener('input', function (event) {
+  console.log('Color scheme changed to', event.target.value);
+
+
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+});
