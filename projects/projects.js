04f56540defa4a0c8febba8fc178ff635491d70b
banffjiang <banffjiang327@gmail.com>
2025-01-27 16:48:41 -0800

    idk

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects } from "global.js";
+import { fetchJSON, renderProjects } from "../globaL.js";
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
