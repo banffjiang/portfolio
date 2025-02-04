57aa91a44fbf617070f77b189699b1f93a0f5e0d
banffjiang <banffjiang327@gmail.com>
2025-02-04 15:10:43 -0800

    step 1

--- projects/projects.js
+++ projects/projects.js
@@ -11,3 +11,35 @@ if (projectsTitle) {
 
 }
 
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
+
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+
+let data = [1, 2];
+
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr(colors[idx]) 
+})
+
+let colors = ['gold', 'purple'];
