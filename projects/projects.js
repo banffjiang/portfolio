7c7712c4a4b89c78b5f093ad75be4554f6c9f099
banffjiang <banffjiang327@gmail.com>
2025-02-04 15:30:11 -0800

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -11,31 +11,20 @@ if (projectsTitle) {
 
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
-let data = [1, 2];
+let data = [1, 2, 3, 4, 5, 5];
 
-let total = 0;
-for (let d of data) {
-  total += d;
-}
+let sliceGenerator = d3.pie();
 
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
-
-let colors = ['gold', 'purple'];
+let arcData = sliceGenerator(data);
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let arcs = arcData.map((d) => arcGenerator(d));
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-arcs.forEach((arc, idx) => {
-    d3.select('svg')
-      .append('path')
-      .attr('d', arc)
-      .attr('fill', colors[idx]);
+let svg = d3.select('svg');
+
+arcData.forEach((d, idx) => {
+    svg.append('path')
+        .attr('d', arcGenerator(d))
+        .attr('fill', colors(idx)); 
 });
