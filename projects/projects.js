2621aca391c4820ffd6707bdda8c17c4ee2968bc
banffjiang <banffjiang327@gmail.com>
2025-02-04 16:19:23 -0800

    3

--- projects/projects.js
+++ projects/projects.js
@@ -11,15 +11,15 @@ if (projectsTitle) {
 
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm";
 
-let data = [
-    { value: 1, label: 'apples' },
-    { value: 2, label: 'oranges' },
-    { value: 3, label: 'mangos' },
-    { value: 4, label: 'pears' },
-    { value: 5, label: 'limes' },
-    { value: 5, label: 'cherries' },
-  ];
+let rolledData = d3.rollups(
+    projects,
+    (v) => v.length,
+    (d) => d.year,
+  );
 
+let data = rolledData.map(([year, count]) => {
+    return { value: count, label: year };
+  });
 
 
 let sliceGenerator = d3.pie().value((d) => d.value);
